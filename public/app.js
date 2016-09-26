db48b7cb3a43fcdbbda8dad8e3553cbfcb5ce3ad
siansrd <sianrobinsondavies@gmail.com>
2016-09-26 19:24:55 +0100

    Add image element to add cat method.

--- public/app.js
+++ public/app.js
@@ -1,14 +1,16 @@
 var app = function() {
-  addCat("Dog", "Chocolate");
+  addCat("Dog", "Chocolate", "http://static.boredpanda.com/blog/wp-content/uploads/2014/02/funny-wet-cats-1.jpg");
 }
 
-var addCat = function(name, food) {
+var addCat = function(name, food, url) {
   var cat = createUlCat();
   var nameItem = createLiName(name);
   var faveFood = createLiFood(food);
+  var photo = createLiImage(url);
 
   cat.appendChild(nameItem);
-  cat.appendChild(favFood);
+  cat.appendChild(faveFood);
+  cat.appendChild(photo);
   var cats = document.querySelector('#cats');
   cats.appendChild(cat);
 }
@@ -16,34 +18,28 @@ var addCat = function(name, food) {
 
 // 1. Create ul Cat
 var createUlCat = function() {
-  cat = document.createElement("UL");
+  var cat = document.createElement("UL");
   cat.className = "cat";
   return cat;
 }
-
-
 // 2. Create li name
 var createLiName = function(name) {
-  nameItem = document.createElement("LI");
+  var nameItem = document.createElement("LI");
   nameItem.appendChild(document.createTextNode("Name: " + name));
   return nameItem;
 }
 // 3. Create li favefood
 var createLiFood = function(food) {
-  favFood = document.createElement("LI");
+  var favFood = document.createElement("LI");
   favFood.appendChild(document.createTextNode("Favourite food: " + food));
   return favFood;
 }
 // 4. Create image
-
-
+var createLiImage = function(url) {
+  var catPic = document.createElement("IMG");
+  catPic.width = '500';
+  catPic.src = url;
+  return catPic;
+}
 
 window.onload = app;
-
-// var x = document.createElement("UL");
-// var x = document.createElement("LI");
-// var x = document.getElementById("myUL");
-// var x = document.getElementById("myLi");
-// var x = document.getElementById("myImg");
-// var x = document.createElement("IMG");
-// imageObject.width=pixels
